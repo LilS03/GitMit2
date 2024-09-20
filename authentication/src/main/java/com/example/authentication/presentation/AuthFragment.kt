0bb6833144117568bf4a13ec625f5c6b7555f681
lils <lilsolkaryan@gmail.com>
2024-09-20 19:25:11 +0400

    Authentication layer

--- authentication/src/main/java/com/example/authentication/presentation/AuthFragment.kt
+++ authentication/src/main/java/com/example/authentication/presentation/AuthFragment.kt
@@ -1,10 +1,11 @@
-package com.example.authentication
+package com.example.authentication.presentation
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.authentication.R
 
 class AuthFragment : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
