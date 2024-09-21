3e65f013b0a3aab070c8164dde634ed83aa6647a
lils <lilsolkaryan@gmail.com>
2024-09-21 12:09:07 +0400

    without di

--- app/src/main/java/com/example/gitmit/MainActivity.kt
+++ app/src/main/java/com/example/gitmit/MainActivity.kt
@@ -1,29 +1,29 @@
 package com.example.gitmit
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.platform.LocalContext
+import com.example.authentication.presentation.LoginActivity
 import com.example.gitmit.ui.theme.GitMitTheme
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             GitMitTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    LoginActivityContainer()
                 }
             }
         }
@@ -31,17 +31,7 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    GitMitTheme {
-        Greeting("Android")
-    }
+fun LoginActivityContainer() {
+    val context = LocalContext.current
+    context.startActivity(Intent(context, LoginActivity::class.java))
 }
--- authentication/src/main/java/com/example/authentication/presentation/AuthFragment.kt
+++ authentication/src/main/java/com/example/authentication/presentation/AuthFragment.kt
@@ -7,28 +7,42 @@ import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
 import com.example.authentication.R
+import com.example.authentication.data.utils.SharedPreferencesHelper
 
 class AuthFragment : Fragment() {
-    private lateinit var viewModel: AuthViewModel
-    private lateinit var token: EditText
-    private lateinit var logIn: Button
+
+    private lateinit var etToken: EditText
+    private lateinit var btnLogIn: Button
+    private lateinit var sharedPreferencesHelper: SharedPreferencesHelper
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_auth, container, false)
-    }
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        viewModel = ViewModelProvider(this)[AuthViewModel::class.java]
-        initViews(view)
-    }
-    private fun initViews(view: View) {
-        token = view.findViewById(R.id.etToken)
-        logIn = view.findViewById(R.id.btnLogin)
+        val view = inflater.inflate(R.layout.fragment_auth, container, false)
+
+        etToken = view.findViewById(R.id.etToken)
+        btnLogIn = view.findViewById(R.id.btnLogin)
+        sharedPreferencesHelper = SharedPreferencesHelper(requireContext())
+
+        btnLogIn.setOnClickListener {
+            val token = etToken.text.trim().toString()
+            if (token.isNotEmpty()) {
+                sharedPreferencesHelper.saveToken(token)
+                //openActivity()
+            } else {
+                etToken.error = "Token cannot be empty"
+            }
+        }
+
+        return view
     }
 
+    // in this case i will open another activity
+//    private fun openActivity() {
+//        val intent = Intent(activity, Activity::class.java)
+//        startActivity(intent)
+//        activity?.finish()
+//    }
 }
