34dfdff17470ae3c57df800634418863b3f118f2
lils <lilsolkaryan@gmail.com>
2024-10-14 11:10:25 +0400

    simple changes

--- authentication/src/main/java/com/example/authentication/data/retrofit/GitHubService.kt
+++ authentication/src/main/java/com/example/authentication/data/retrofit/GitHubService.kt
@@ -1,7 +1,6 @@
 package com.example.authentication.data.retrofit
 
-import com.example.authentication.data.model.UserDto
-import retrofit2.Response
+import com.example.core.data.model.UserDto
 import retrofit2.http.GET
 import retrofit2.http.Header
 
--- core/data/src/main/java/com/example/core/data/model/UserDto.kt
+++ core/data/src/main/java/com/example/core/data/model/UserDto.kt
@@ -1,4 +1,4 @@
-package com.example.authentication.data.model
+package com.example.core.data.model
 import com.google.gson.annotations.SerializedName
 
 data class UserDto(
